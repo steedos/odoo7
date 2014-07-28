8ec0eb40fcab4c11427a118781be735f6390b3ca
steedos <zhuangjianguo@hotlong.com>
2014-07-28 13:50:26 +0800

    OEM

--- openerp/addons/web/controllers/main.py
+++ openerp/addons/web/controllers/main.py
@@ -541,8 +541,8 @@ html_template = """<!DOCTYPE html>
     <head>
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
         <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-        <title>OpenERP</title>
-        <link rel="shortcut icon" href="/web/static/src/img/favicon.ico" type="image/x-icon"/>
+        <title>SteedOS</title>
+        <link rel="shortcut icon" href="http://www.steedos.com/website/img/steedos/AppIcon76x76.png" type="image/png"/>
         <link rel="stylesheet" href="/web/static/src/css/full.css" />
         %(css)s
         %(js)s
